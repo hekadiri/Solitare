6bbca4b31caca3d5b2273e88455579ea1375e0c3
hekadiri <hekadiri@umich.edu>
2021-08-05 18:37:47 -0400

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Hassan Kadiri on 8/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
